05889be7bdbd93b39c0788ada15fbdd169d24d45
elraine <laevatinn@gmail.com>
2016-02-05 14:57:45 +0100

    Adding new View Controllers

---
+++ Conferencia/MapsViewController.swift
@@ -0,0 +1,13 @@
+//
+//  MapsViewController.swift
+//  Conferencia
+//
+//  Created by Alexis Chan on 05/02/2016.
+//  Copyright © 2016 achan. All rights reserved.
+//
+
+import UIKit
+
+class MapsViewController: UIViewController {
+
+}
