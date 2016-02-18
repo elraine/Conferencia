885cde7839331632a67bf0c0991d5cdec208e31d
elraine <laevatinn@gmail.com>
2016-02-18 09:00:07 +0100

    Yay

--- Conferencia/MapsViewController.swift
+++ Conferencia/MapsViewController.swift
@@ -8,44 +8,69 @@
 
 import UIKit
 import MapKit
-//import CoreLocation
+import CoreLocation
+
 
 class MapsViewController: UIViewController,CLLocationManagerDelegate,MKMapViewDelegate {
 
-    @IBOutlet weak var mapView: MKMapView!
-
+    
+    @IBOutlet weak var displayCoords: UILabel!
+    
+    @IBOutlet weak var mapView: MKMapView! = MKMapView()
+    
     let initialLocation = CLLocation(latitude:44.8058523, longitude:-0.6066426)
     let regionRadius: CLLocationDistance = 1000
+    
     func centerMapOnLocation(location: CLLocation) {
         let coordinateRegion = MKCoordinateRegionMakeWithDistance(location.coordinate,
             regionRadius * 2.0, regionRadius * 2.0)
         mapView.setRegion(coordinateRegion, animated: true)
-    }
-  //  let locationManager = CLLocationManager()
+           }
+    //user location
+  let locationManager = CLLocationManager()
+    
+    
+    
     
     override func viewDidLoad(){
         super.viewDidLoad()
-        mapView.showsUserLocation = true
         
         //location manager update
-//            self.locationManager.requestWhenInUseAuthorization()
-//            if CLLocationManager.locationServicesEnabled() {
-//                self.locationManager.delegate = self
-//                self.locationManager.desiredAccuracy = kCLLocationAccuracyNearestTenMeters
-//                self.locationManager.startUpdatingLocation()
-//        }
+          self.locationManager.requestWhenInUseAuthorization()
+            if CLLocationManager.locationServicesEnabled() {
+                self.locationManager.delegate = self
+                self.locationManager.requestLocation()
+        }
+        
+        
         //map update
+        mapView.delegate = self
+        mapView.showsUserLocation = true
+//        var region = MKCoordinateRegion()
+//        region.center.latitude = locationManager.location!.coordinate.latitude
+//        region.center.longitude = locationManager.location!.coordinate.longitude
+//        region.span = MKCoordinateSpanMake(0.2, 0.2)
+//        mapView.setRegion(region, animated: true)
         centerMapOnLocation(initialLocation)
     }
 
     
-//    func locationManager(manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-//        let locValue:CLLocationCoordinate2D = manager.location!.coordinate
-//        print("locations = \(locValue.latitude) \(locValue.longitude)")
-//    }
-//    
-//    func locationManager(manager: CLLocationManager, didFailWithError error: NSError) {
-//        print("Failed to find user's location: \(error.localizedDescription)")
-//    }
+    func locationManager(manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let locValue:CLLocationCoordinate2D = manager.location!.coordinate
+        //print("location = \(locValue.latitude) \(locValue.longitude)")
+        
+        displayCoords.text = "latitude : \(locValue.latitude)" + "\n long : \(locValue.longitude)"
+    }
 
+    func locationManager(manager: CLLocationManager, didFailWithError error: NSError) {
+        print("Failed to find user's location: \(error.localizedDescription)")
+    }
+
+    
+    @IBAction func prepareForUnwind(unwindSegue: UIStoryboardSegue){
+        if let _ = unwindSegue.sourceViewController as? MapsPreludeController{
+        NSLog ("INCOMING")
+        }
+    }
+    
 }
