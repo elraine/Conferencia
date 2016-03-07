71446d1489d3b0609b221fde07c56d2c331a32a2
elraine <laevatinn@gmail.com>
2016-03-07 10:02:47 +0100

    ffff

---
+++ Conferencia/LoadingOverlay.swift
@@ -0,0 +1,49 @@
+//
+//  LoadingOverlay.swift
+//  Conferencia
+//
+//  Created by Alexis Chan on 07/03/2016.
+//  Copyright © 2016 achan. All rights reserved.
+//
+
+import UIKit
+
+public class LoadingOverlay{
+    
+    var overlayView : UIView!
+    var activityIndicator : UIActivityIndicatorView!
+    
+    class var shared: LoadingOverlay {
+        struct Static {
+            static let instance: LoadingOverlay = LoadingOverlay()
+        }
+        return Static.instance
+    }
+    
+    init(){
+        self.overlayView = UIView()
+        self.activityIndicator = UIActivityIndicatorView()
+        
+        overlayView.frame = CGRectMake(0, 0, 80, 80)
+        overlayView.backgroundColor = UIColor(white: 0, alpha: 0.7)
+        overlayView.clipsToBounds = true
+        overlayView.layer.cornerRadius = 10
+        overlayView.layer.zPosition = 1
+        
+        activityIndicator.frame = CGRectMake(0, 0, 40, 40)
+        activityIndicator.center = CGPointMake(overlayView.bounds.width / 2, overlayView.bounds.height / 2)
+        activityIndicator.activityIndicatorViewStyle = .WhiteLarge
+        overlayView.addSubview(activityIndicator)
+    }
+    
+    public func showOverlay(view: UIView) {
+        overlayView.center = view.center
+        view.addSubview(overlayView)
+        activityIndicator.startAnimating()
+    }
+    
+    public func hideOverlayView() {
+        activityIndicator.stopAnimating()
+        overlayView.removeFromSuperview()
+    }
+}
