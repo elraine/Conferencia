b748af97a00985a3db92abb528b4cfea8b527410
elraine <laevatinn@gmail.com>
2016-03-07 10:02:38 +0100

    ff

--- Conferencia/HomepageViewController.swift
+++ Conferencia/HomepageViewController.swift
@@ -7,14 +7,32 @@
 //
 
 import UIKit
+let CONF_ID = 1601
 
 class HomepageViewController: UIViewController {
 
     @IBOutlet weak var Progress: UIProgressView!
     
+    let SQLBase = SQLiteDataStore.sharedInstance
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        if(!isAppAlreadyLaunchedOnce()){
+            print("first time")
+            LoadingOverlay.showOverlay(self.view! as UIView)
+        do{
+            try SQLBase.createTables()
+        }catch{}
+        
+        SetDataManager(id: CONF_ID)
+            
+        }else{
+            print("not first time")
+        }
+        //resetData()
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -32,4 +50,63 @@ class HomepageViewController: UIViewController {
     }
 
 }
+
+func resetData(){
+    do {
+        if let events = try EventDataHelper.findAll() {
+            for event in events {
+                try EventDataHelper.delete(event)
+                
+            }
+        }
+        if let rooms = try RoomDataHelper.findAll() {
+            for room in rooms {
+                try RoomDataHelper.delete(room)
+                
+            }
+        }
+        if let docs = try DocDataHelper.findAll() {
+            for doc in docs {
+                try DocDataHelper.delete(doc)
+            }
+        }
+        if let authors = try AuthorDataHelper.findAll() {
+            for author in authors {
+                try AuthorDataHelper.delete(author)
+                
+            }
+        }
+        if let affiliations = try AffiliationDataHelper.findAll() {
+            for affiliation in affiliations {
+                try AffiliationDataHelper.delete(affiliation)
+                
+            }
+        }
+        if let locateds = try LocatedDataHelper.findAll() {
+            for located in locateds {
+                try LocatedDataHelper.delete(located)
+                
+            }
+        }
+        if let comeFroms = try ComeFromDataHelper.findAll() {
+            for comeFrom in comeFroms {
+                try ComeFromDataHelper.delete(comeFrom)
+                
+            }
+        }
+    } catch _ {}
+}
+    
+    func isAppAlreadyLaunchedOnce()->Bool{
+        let defaults = NSUserDefaults.standardUserDefaults()
+        
+        if let _ = defaults.stringForKey("isAppAlreadyLaunchedOnce"){
+            print("App already launched")
+            return true
+        }else{
+            defaults.setBool(true, forKey: "isAppAlreadyLaunchedOnce")
+            print("App launched first time")
+            return false
+        }
+    }
 
--- Conferencia/ProgramViewController.swift
+++ Conferencia/ProgramViewController.swift
@@ -12,20 +12,125 @@ import Alamofire
 import SwiftyJSON
 
 
-class ProgramViewController: UIViewController{
+class ProgramViewController: UIViewController, UITableViewDelegate, UITableViewDataSource,UIPickerViewDelegate,UIPickerViewDataSource{
     
- let SQLBase = SQLiteDataStore.sharedInstance
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    let SQLBase = SQLiteDataStore.sharedInstance
+    var events : [Event]? = []
+    
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var ctrlBar: UISegmentedControl!
+    
+    @IBOutlet weak var picker: UIPickerView!
+    
+    
+    // MARK: - Table view data source
+    
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
     }
+    
+     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return events!.count
+    }
+    
+    
+     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("eventCell", forIndexPath: indexPath) as! EventViewCell
+        cell.type.text = events![indexPath.row].type
+        cell.timeS.text = events![indexPath.row].time_start
+        
+        cell.timeE.text = events![indexPath.row].time_end
+        cell.name.text = events![indexPath.row].title
+        
+        
+        if events![indexPath.row].roomid == -1{
+            cell.room.text = ""
+        }else{
+            let room = try? RoomDataHelper.find(events![indexPath.row].roomid)
+             cell.room.text = room!!.name
+        }
+        return cell
+    }
+    
 
+    
+    
+    //PICKER TIME
+    
+    
+    var source = ["default", "data","try"]
+    
+    func numberOfComponentsInPickerView(pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return source.count
+    }
+    func pickerView(pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        do{
+            events = try EventDataHelper.findEventByDay(source[row])
+        }catch{}
+        self.tableView.reloadData()
+        return dateView(source[row])
+    }
+    
+    //VIEWS EVENTS
+    override func viewDidLoad(){
+        super.viewDidLoad()
+        
+        
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        self.tableView.registerClass(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        
+        do{
+            source = try EventDataHelper.findDaySlot()!
+        }catch{
+            print("source fail")
+        }
+       /*
+        do{
+            events = try EventDataHelper.findAll()
+        }catch{}
+        */
+        
+        /*
+        //choose date
+        do{ctrlBar.replaceSegments(try EventDataHelper.findTimeSlotByDay()!)
+        }catch{
+        print("array error")
+        }*/
+        
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
+    //Helped Methodes
+    func dateView(date : String) -> String{
+        var myArray = date.componentsSeparatedByString("-")
+        
+        let timeStartComponents = NSDateComponents()
+        timeStartComponents.year = Int(myArray[0])!
+        timeStartComponents.month = Int(myArray[1])!
+        timeStartComponents.day = Int(myArray[2])!
+        
+        let timeStart = NSCalendar.currentCalendar().dateFromComponents(timeStartComponents)!
+        
+        let dayTimePeriodFormatter = NSDateFormatter()
+        dayTimePeriodFormatter.dateFormat = "EE d"
+        
+        return dayTimePeriodFormatter.stringFromDate(timeStart)
+    }
 }
+
+
 
